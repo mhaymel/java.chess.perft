9d629f42934badd35114f414272fcc2939fbb962
Markus Heumel <markus.heumel@gmx.at>
2025-12-05 12:24:21 +0100

    king moves tests

--- src/test/java/com/haymel/chess/perft/GenTest.java
+++ src/test/java/com/haymel/chess/perft/GenTest.java
@@ -30,22 +30,31 @@ final class GenTest {
          );
    }
 
-   static Stream<Arguments> kingsMoves() {
+   static Stream<Arguments> whiteKingsMoves() {
       return Stream.of(
          // White king moves
-         of("k7/8/8/8/4K3/8/8/8 w - - 1 1",Set.of("e4e5", "e4f5", "e4f4", "e4f3", "e4e3", "e4d3", "e4d4", "e4d5")),
-         of("k7/8/8/8/8/8/8/K7 w - - 1 1", Set.of("a1a2", "a1b2", "a1b1")),
-         of("k7/8/8/8/8/8/8/7K w - - 1 1", Set.of("h1h2", "h1g1", "h1g2")),
-         of("k6K/8/8/8/8/8/8/8 w - - 1 1", Set.of("h8h7", "h8g7", "h8g8")),
-         of("K7/8/8/8/8/8/8/7k w - - 1 1", Set.of("a8b8", "a8b7", "a8a7"))
-         // Black king moves
-//         of("4k3/8/8/8/Pp6/8/8/4K3 b - a3 1 1", Set.of("b4a3",         /*king*/ "e8d8", "e8d7", "e8e7", "e8f8", "e8f7")),
-//         of("4k3/8/8/8/Pp6/8/8/4K3 b - b3 1 1", Set.of("a4b3", "c4b3", /*king*/ "e8d8", "e8d7", "e8e7", "e8f8", "e8f7")),
-//         of("4k3/8/8/8/Pp6/8/8/4K3 b - h3 1 1", Set.of("g4h3",         /*king*/ "e8d8", "e8d7", "e8e7", "e8f8", "e8f7")),
-//         of("4k3/8/8/8/Pp6/8/8/4K3 b - g3 1 1", Set.of("f4g3", "h4g3", /*king*/ "e8d8", "e8d7", "e8e7", "e8f8", "e8f7"))
+         of("k7/8/8/8/4K3/8/8/8 w - - 1 1",  Set.of("e4e5", "e4f5", "e4f4", "e4f3", "e4e3", "e4d3", "e4d4", "e4d5")),
+         of("k7/8/8/8/8/8/8/K7 w - - 1 1",   Set.of("a1a2", "a1b2", "a1b1")),
+         of("k7/8/8/8/8/8/8/7K w - - 1 1",   Set.of("h1h2", "h1g1", "h1g2")),
+         of("k6K/8/8/8/8/8/8/8 w - - 1 1",   Set.of("h8h7", "h8g7", "h8g8")),
+         of("K7/8/8/8/8/8/8/7k w - - 1 1",   Set.of("a8b8", "a8b7", "a8a7")),
+         of("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", Set.of()),
+         of("4k3/8/8/3ppp2/3pKp2/3ppp2/8/8 w - - 0 1", Set.of("e4e5", "e4f5", "e4f4", "e4f3", "e4e3", "e4d3", "e4d4", "e4d5"))
       );
    }
 
+   static Stream<Arguments> blackKingsMoves() {
+      return Stream.of(
+         // White king moves
+         of("8/8/8/4k3/8/8/8/7K b - - 0 1",  Set.of("e5e6", "e5f6", "e5f5", "e5f4", "e5e4", "e5d4", "e5d5", "e5d6")),
+         of("k7/8/8/8/8/8/8/K7 b - - 0 1",   Set.of("a8b8", "a8b7", "a8a7")),
+         of("7k/8/8/8/8/8/8/K7 b - - 0 1",   Set.of("h8h7", "h8g7", "h8g8")),
+         of("8/8/8/8/8/8/8/K6k b - - 0 1 ",   Set.of("h1g1", "h1g2", "h1h2")),
+         of("7K/8/8/8/8/8/8/k7 b - - 0 1",   Set.of("a1a2", "a1b2", "a1b1")),
+         of("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1", Set.of()),
+         of("7K/8/3PPP2/3PkP2/3PPP2/8/8/8 b - - 0 1", Set.of("e5e6", "e5f6", "e5f5", "e5f4", "e5e4", "e5d4", "e5d5", "e5d6"))
+      );
+   }
 
    @ParameterizedTest
    @MethodSource("enPassantPositions")
@@ -54,8 +63,14 @@ final class GenTest {
    }
 
    @ParameterizedTest
-   @MethodSource("kingsMoves")
-   void kingsMoves(String fen, Set<String> expectedMoves) {
+   @MethodSource("whiteKingsMoves")
+   void whiteKingsMoves(String fen, Set<String> expectedMoves) {
+      test(fen, expectedMoves);
+   }
+
+   @ParameterizedTest
+   @MethodSource("blackKingsMoves")
+   void blackKingsMoves(String fen, Set<String> expectedMoves) {
       test(fen, expectedMoves);
    }
 
