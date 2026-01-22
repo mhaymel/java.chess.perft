d941f0e696131a88a50673a66b12befb23fefc78
markus <markus.heumel@gmx.at>
2026-01-22 19:53:08 +0100

    MinimalUciTest added

---
+++ src/test/java/com/haymel/chess/uci/MinimalUciTest.java
@@ -0,0 +1,6 @@
+package com.haymel.chess.uci;
+
+final class MinimalUciTest {
+//   position startpos moves e2e3 e7e5 g1f3 e5e4 f3d4 d7d5 b1c3 g8f6 a1b1 c7c5 f1b5 e8e7 d4b3 c5c4 b3d4 a7a6 b5a4 b7b5 b2b3 b5a4 b3c4 e7e8 c4d5 c8g4 f2f3 e4f3 g2f3 g4h3 h1g1 f6d5 c3d5 d8d5 b1b2 f8e7 g1g3 e7h4 c2c3 b8c6 d1a4 h4g3 h2g3 h3d7 a4b3 d5h5 d4c6 h5h1 e1f2 d7c6 b3b8 a8b8 b2b8 e8d7 b8h8 h1h2 f2f1 c6b5 c3c4 b5c4
+//   go wtime 111320 btime 126332 winc 0 binc 0
+}
